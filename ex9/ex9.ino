7a3c6dbed598095bc6e5a931a274da0aa8682daa
Rodolfo Cossovich <todocono@gmail.com>
2015-12-03 11:56:05 +0800

    corrected ex9

--- ex9/ex9.ino
+++ ex9/ex9.ino
@@ -1,19 +1,15 @@
 /*
-HC-SR04 Ping distance sensor]
-VCC to arduino 5v GND to arduino GND
-Echo to Arduino pin 13 Trig to Arduino pin 12
-Red POS to Arduino pin 11
-Green POS to Arduino pin 10
-560 ohm resistor to both LED NEG and GRD power rail
-More info at: http://goo.gl/kJ8Gl
-Original code improvements to the Ping sketch sourced from Trollmaker.com
-Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+Example 9
+   Introduction to Robotics
+   by Rodolfo Cossovich at New York University
+   Description: This software uses HC-SR04 on pins 13 and 12
+   
+   Original code improvements to the Ping sketch sourced from Trollmaker.com . More info at: http://goo.gl/kJ8Gl
+   Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
 */
 
 #define trigPin 13
 #define echoPin 12
-#define led 11
-#define led2 10
 
 void setup() {
   Serial.begin (9600);
@@ -34,12 +30,10 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
   if (distance < 4) {  // This is where the LED On/Off happens
-    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
-  digitalWrite(led2,LOW);
+   Serial.print("really close!");
 }
   else {
-    digitalWrite(led,LOW);
-    digitalWrite(led2,HIGH);
+   //do nothing
   }
   if (distance >= 200 || distance <= 0){
     Serial.println("Out of range");
